3a1146ec46ec98e700ae4b97173b1e248b4d190c
Tufahel <tufahel96@gmail.com>
2022-06-15 21:16:56 +0600

    validation and signup create

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # GET / about
   get "about", to: "about#index"
   
+  get "sign_up", to: "registrations#new"
+
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
 end
