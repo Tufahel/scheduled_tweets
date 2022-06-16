b50118b98d7e28ec935ae6508d2872533cd3f079
Tufahel <tufahel96@gmail.com>
2022-06-16 18:14:20 +0600

    fixed user.find_by by giving id and implement loggin out

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
   
   get "sign_up", to: "registrations#new"
-
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
 end
