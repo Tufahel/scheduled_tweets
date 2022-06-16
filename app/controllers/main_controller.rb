2f91aeaf0ba7243ce50be4a3816850437367d427
Tufahel <tufahel96@gmail.com>
2022-06-16 16:49:54 +0600

    login with session cookie

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
     def index
-        flash[:notice] = "logged in successfully"
-        flash[:alert] = "invalid email or password"
+        if session[:user_id]
+            @user = User.find_by(session[:user_id])
+        end
     end
 end
